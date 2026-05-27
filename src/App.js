3690b6053a064602451f863076dee84d98ff3d8b
JULIA ISABELA DE CASTRO SILVA <Lu1zxp199@gmail.com>
2026-05-27 17:12:59 -0300

    mais

--- src/App.js
+++ src/App.js
@@ -2,8 +2,7 @@ import React from "react";
 import { Route, BrowserRouter, Routes } from "react-router-dom";
 import Trainer from "./pages/inicio";
 import Poke from "./pages/Poke";
-import Pokep2 from "./pages/Pokep2";
-import PokePrincipal from "./pages/PokePrincipal"
+
 
 function App() {
     return (
